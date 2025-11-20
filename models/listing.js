62dc3a03a11f7551b9605f71a69ef8c7a9395d9c
Bibek Bhattarai <bibek.bhattarai072@gmail.com>
2025-11-20 14:11:19 -0500

    Handled delete Listing

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js")
 
 const listingSchema = new Schema({
     title: {
@@ -21,6 +22,14 @@ const listingSchema = new Schema({
             ref: "Review"
         },
     ],
+});
+
+//post mongoose middleware to delete the review from database (this middleware run after deletion of listing)
+//first receives the deleted listing as listing(listing.title, listing.price, listing.reviews->contain array of review ObjectIDs)
+listingSchema.post("findOneAndDelete", async (listing)=> { 
+    if(listing) {
+        await Review.deleteMany({_id : {$in: listing.reviews}}); //this removes all review documents where _id is inside the listings reviews array.
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
