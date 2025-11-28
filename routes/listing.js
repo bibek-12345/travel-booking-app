b2b1e9052b6d8ceb1a5e07b0d5af59bd0726d6c5
Bibek Bhattarai <bibek.bhattarai072@gmail.com>
2025-11-27 21:29:15 -0500

    cloud setup done

--- routes/listing.js
+++ routes/listing.js
@@ -2,29 +2,27 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const listingController = require("../controllers/listings.js");
+const multer  = require('multer');
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage })
 
-const listingController = require("../controllers/listings.js")
+router.route("/")
+    .get(wrapAsync(listingController.index)) //Index route: to view all the listings
+    .post(isLoggedIn, validateListing, upload.single("listing[image]"), wrapAsync(listingController.createListing)), //Create Route or post route: 
 
-//Index route: to view all the listings
-router.get("/", wrapAsync(listingController.index));
 
 //New Route: this route gives the form to add the new listing
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-//Show route: to read the data or view the data 
-router.get("/:id", wrapAsync(listingController.showListing));
-
-//Create Route or post route: 
-router.post("/", isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+router.route("/:id")
+    .get(wrapAsync(listingController.showListing)) //Show route: to read the data or view the data 
+    .put(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing)) //Update Route: this takes the data which was submitted through form and update in database
+    .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing)); //Delete Route: this will delete/destroy the individual listing
 
 
 //Edit Route: it is used to give form to edit in response
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
 
-//Update Route: this takes the data which was submitted through form and update in database
-router.put("/:id", isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing));
-
-//Delete Route: this will delete/destroy the individual listing
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,17 +5,14 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController = require("../controllers/users.js");
 
-//route: just to render the signup form
-router.get("/signup", userController.renderSignupForm);
+router.route("/signup")
+    .get(userController.renderSignupForm) //route: just to render the signup form
+    .post(wrapAsync(userController.signup)); //route that takes post request and store data into the database, signup
 
-//route that takes post request and store data into the database, signup
-router.post("/signup", wrapAsync(userController.signup));
 
-//login route: just to render the login form
-router.get("/login", userController.renderLoginForm);
-
-//login route that takes post request and check the form filled data from the database
-router.post("/login", saveRedirectUrl, passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), userController.login);
+router.route("/login")
+    .get(userController.renderLoginForm)  //login route: just to render the login form
+    .post(saveRedirectUrl, passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), userController.login); //login route that takes post request and check the form filled data from the database
 
 //logout route
 router.get("/logout", userController.logout);
